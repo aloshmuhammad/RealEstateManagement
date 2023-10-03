8220a166159cbfdfeab08a0f83ee9641e56cc197
Alosh Muhammad <alosh.km@gmail.com>
2023-10-03 09:41:48 +0530

    page route added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
-
+import { BrowserRouter as Router ,Routes,Route } from 'react-router-dom'
+import About from './pages/about'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import Signout from './pages/Signout'
+import Signin from './pages/Signin'
 const App = () => {
   return (
     <>
-    <h1 className='text-red-100'>App</h1>
+  <Router>
+<Routes>
+  <Route path='/' element={<Home/>}/>
+  <Route path='/about' element={<About/>}/>
+  <Route path='/sign-in' element={<Signin/>}/>
+  <Route path='/Sign-out' element={<Signout/>}/>
+
+</Routes>
+
+  </Router>
     
     </>
   )
